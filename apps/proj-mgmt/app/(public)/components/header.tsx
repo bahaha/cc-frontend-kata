7ece0146d59f2335bbcd1d3731250bece335f5dd
clay_cheng <devclaycheng@gmail.com>
2024-11-18 13:02:31 +0700

    collapsed backdrop filter effect

--- apps/proj-mgmt/app/(public)/components/header.tsx
+++ apps/proj-mgmt/app/(public)/components/header.tsx
@@ -1,12 +1,47 @@
 import Image from "next/image";
+import { cva, type VariantProps } from "class-variance-authority";
 
 import { Menu } from "./menu";
 
-type HeaderPops = {};
+const headerVariant = cva(
+  [
+    "group fixed bg-background px-3",
+    "inset-x-0 inset-y-[--header-top] max-w-[--header-width] mx-auto rounded-xl",
+  ],
+  {
+    variants: {
+      backdrop: {
+        default: [
+          "supports-[backdrop-filter]:bg-background/60",
+          "supports-[backdrop-filter]:backdrop-blur-xl",
+          "supports-[backdrop-filter]:backdrop-saturate-150",
+        ],
+      },
+      border: {
+        default: [
+          "before:absolute before:inset-0 before:border before:rounded-xl",
+          "before:pointer-events-none",
+        ],
+      },
+      collapseState: {
+        default: [
+          "before:h-[--header-height] [clip-path:var(--header-collapse-clip)]",
+        ],
+      },
+    },
+    defaultVariants: {
+      backdrop: "default",
+      border: "default",
+      collapseState: "default",
+    },
+  },
+);
 
-export function Header({}: HeaderPops) {
+type HeaderPops = {} & VariantProps<typeof headerVariant>;
+
+export function Header(props: HeaderPops) {
   return (
-    <header>
+    <header className={headerVariant(props)}>
       Header works
       <Image width="24" height="24" src="/logo.svg" alt="CC's Proj Mgmt" />
       <Menu />
--- apps/proj-mgmt/tailwind.config.ts
+++ apps/proj-mgmt/tailwind.config.ts
@@ -2,11 +2,18 @@ import type { Config } from "tailwindcss";
 import baseConfig from "@cc/tailwind-config/tailwind.config";
 
 const config: Config = {
-  ...baseConfig,
   presets: [baseConfig],
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "../../packages/ui/src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  theme: {
+    extend: {
+      padding: {
+        "page-left": "var(--page-padding-left)",
+        "page-right": "var(--page-padding-right)",
+      },
+    },
+  },
 };
 export default config;
